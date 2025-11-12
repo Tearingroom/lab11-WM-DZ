0562a4973c5bb8c76d0797b7a6716d5592a2d806
Will Munro <91396342+Tearingroom@users.noreply.github.com>
2025-11-12 15:20:48 -0500

    firstcommit

--- calculator.py
+++ calculator.py
@@ -4,8 +4,51 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
 
+"""
+calculator.py
+- Defines functions used to create a simple calculator
 
+One function per operation, in order.
+"""
+import math
+def square_root(a):
+    try:
+        if a < 0:
+            raise ValueError("Cannot take the square root of a negative number.")
+        return math.sqrt(a)
+    except TypeError:
+        raise TypeError("Input must be a number.")
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except TypeError:
+        raise TypeError("Inputs must be numbers.")
+
+def add(a, b):
+    return (a+b)
+
+def sub(a,b):
+    return (a-b)
+
+def mul(a,b):
+    return a*b
+
+def div(a, b):
+    try:
+        result = b / a
+        return result
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+        return None
+
+def log(a, b):
+    try:
+        result = math.log(b, a)
+        return result
+    except ValueError:
+        print("Error: log() arguments must be positive and base cannot be 1.")
+        return None
+def exp(a,b):
+    return (a**b)
